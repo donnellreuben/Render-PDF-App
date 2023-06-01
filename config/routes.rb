83f0ca6d2395effe02bc936d2c8361346c31393d
Donnell Reuben <donnell.reuben@icloud.com>
2023-06-01 14:46:21 -0500

    cleaned up

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,3 @@
 Rails.application.routes.draw do
- 
   get '/generate-pdf', to: 'pdfs#generate', as: 'generate_pdf'
-  # post 'pdfs/generate', to: 'pdfs#generate', as: 'generate_pdf'
-  resources :pdfs
 end
-
-
- # get 'pdfs/generate', to: 'pdfs#generate', as: 'generate_pdf'
